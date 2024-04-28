103ce6033ed0cf0272383e25ad2b4b6b890def99
AbdelBt <abdella.boutaarourt@hotmail.com>
2024-04-28 23:33:05 +0200

    host

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app .use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://lucky-cat-92b61e.netlify.app",
         methods: ["GET", "POST"],
     },
 });
