a132aa3bb0aa23b7b5696261168a18da91840f00
AbdelBt <abdella.boutaarourt@hotmail.com>
2024-04-28 22:50:24 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import io from 'socket.io-client'
 import Chat from './Chat';
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://chatapp-bex0.onrender.com")
 
 function App() {
   const [username, setUsername] = useState("");
