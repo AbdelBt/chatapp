d569b8787d19f45ecf3b1bc323e61d67b673a64a
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-05-28 19:02:53 +0200

    FIX

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require("socket.io")
-app .use(cors());
+app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://lucky-cat-92b61e.netlify.app",
+        origin: "https://vocal-bublanina-a2315e.netlify.app",
         methods: ["GET", "POST"],
     },
 });
@@ -17,23 +17,23 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log(`user Connected: ${socket.id}`);
 
-    socket.on("join_room", (data) =>{
+    socket.on("join_room", (data) => {
         socket.join(data)
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
-    socket.on("send_message", (data) =>{
+    socket.on("send_message", (data) => {
         console.log(data)
         socket.to(data.room).emit("receive_message", data);
     });
 
-    socket.on("disconnect", () =>{
+    socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id)
     });
 });
 
 app.get('/', (req, res) => {
     res.send('backend');
-  });
+});
 server.listen(3001, () => {
     console.log("SERVER RUNNING");
 })
